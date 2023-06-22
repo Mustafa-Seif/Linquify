f9f1944ecc65dd85ab8f92c8add71f32bab08a08
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-06-22 13:01:27 +0300

    make app responsive

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-const asideToggle = document.getElementById('asideToggle');
+const asideToggle = document.getElementById("asideToggle");
+const mainNav = document.getElementById("main_nav");
+const asideToggleLogo = document.getElementById("asideToggleLogo");
 
+const handleSideBar = () => {
+  mainNav.classList.toggle("hideSide");
+  document.querySelectorAll(".text-nav").forEach((el) => {
+    el.classList.toggle("hideTextNav");
+  });
+  document.querySelectorAll(".nav_item").forEach((el) => {
+    el.classList.toggle("text-center");
+  });
+  asideToggle.classList.toggle("hideIconHide");
+};
 
-asideToggle.addEventListener('click',()=>{
-    console.log('ff');
-    // document.getElementById('main_nav').classList.toggle('closed-sidebar')
-    // document.getElementById('main_nav').classList.toggle('closed-sidebar')
-    // console.log();
-    // document.querySelectorAll('.text-nav').forEach(el=>{
-    //     el.style.display = 'none'
-    // })
-    // document.querySelectorAll('.nav_item').forEach(el=>{
-    //     el.style.textAlign = 'center'
-    // })
-})
+asideToggle.addEventListener("click", handleSideBar);
+asideToggleLogo.addEventListener("click", handleSideBar);
