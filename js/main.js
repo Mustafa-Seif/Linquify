fc18cc74a62f18a9f32e9bc1cfba787a3cda424f
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-06-22 11:26:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const asideToggle = document.getElementById('asideToggle');
+
+
+asideToggle.addEventListener('click',()=>{
+    console.log('ff');
+    // document.getElementById('main_nav').classList.toggle('closed-sidebar')
+    // document.getElementById('main_nav').classList.toggle('closed-sidebar')
+    // console.log();
+    // document.querySelectorAll('.text-nav').forEach(el=>{
+    //     el.style.display = 'none'
+    // })
+    // document.querySelectorAll('.nav_item').forEach(el=>{
+    //     el.style.textAlign = 'center'
+    // })
+})
