a74da599a8e623b811839b43d1d93be1d1a3bc70
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-07-05 02:26:18 +0300

    fix main section  margin and padding

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@ const mainNav = document.getElementById("main_nav");
 const asideToggleLogo = document.getElementById("asideToggleLogo");
 
 const handleSideBar = () => {
+  
+asideToggleLogo.children[0].style.width='100%'
   mainNav.classList.toggle("hideSide");
   document.querySelectorAll(".text-nav").forEach((el) => {
     el.classList.toggle("hideTextNav");
   });
   document.querySelectorAll(".nav_item").forEach((el) => {
+    el.classList.toggle("justify-content-center");
+  });
+  document.querySelectorAll(".nav_item").forEach((el) => {
     el.classList.toggle("text-center");
   });
-  asideToggle.classList.toggle("hideIconHide");
+  // asideToggle.classList.toggle("hideIconHide");
 };
 
 asideToggle.addEventListener("click", handleSideBar);
 asideToggleLogo.addEventListener("click", handleSideBar);
+
+
+
+
