31d34dd3ae97cf5e45a9999c300eddba2b138a80
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-07-05 12:59:57 +0300

    add flex wrap to card two

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const asideToggle = document.getElementById("asideToggle");
 const mainNav = document.getElementById("main_nav");
 const asideToggleLogo = document.getElementById("asideToggleLogo");
-const logo_wrapper = document.querySelector('.logo_wrapper');
+const logo_wrapper = document.querySelector(".logo_wrapper");
 
 const handleSideBar = () => {
-  
-asideToggleLogo.children[0].style.width='100%'
+  asideToggleLogo.children[0].style.width = "100%";
   mainNav.classList.toggle("hideSide");
   document.querySelectorAll(".text-nav").forEach((el) => {
     el.classList.toggle("hideTextNav");
@@ -16,13 +15,36 @@ asideToggleLogo.children[0].style.width='100%'
   document.querySelectorAll(".nav_item").forEach((el) => {
     el.classList.toggle("text-center");
   });
-  logo_wrapper.classList.toggle('logo_wrapper_center')
+  logo_wrapper.classList.toggle("logo_wrapper_center");
   asideToggle.classList.toggle("hideIconHide");
 };
 
 asideToggle.addEventListener("click", handleSideBar);
 asideToggleLogo.addEventListener("click", handleSideBar);
 
+// let myPromise = new Promise((resolveFun, rejectFun) => {
+//   let connection = 0;
+//   if (connection) {
+//     resolveFun("Connection Success");
+//   } else {
+//     rejectFun("Connection Failed");
+//   }
+// });
+// myPromise
+//   .then((res) => console.log("Then", res))
+//   .catch((err) => console.log("Error", err))
+//   .finally(() => console.log("Final"));
+// console.log('console');
 
 
+async function connection() {
+  let connection = true;
 
+  if (connection) {
+    return 'Connection Success'
+  }else{
+    return 'Connection Failed'
+
+  }
+}
+connection().then((res,err)=> console.log(res))
